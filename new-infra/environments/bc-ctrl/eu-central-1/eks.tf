14d2108084185a891a11077f93f9a646218d95e0
Afonso Costa <afonso3marques@gmail.com>
2026-04-17 00:11:31 +0200

    fix(ctrl): scale bc-ctrl to 3 nodes for indexer anti-affinity; use public.ecr.aws for alpine/busybox init images

--- new-infra/environments/bc-ctrl/eu-central-1/eks.tf
+++ new-infra/environments/bc-ctrl/eu-central-1/eks.tf
@@ -153,9 +153,9 @@ module "eks" {
   eks_managed_node_groups = {
     security = {
       instance_types = ["t3.xlarge"]
-      min_size       = 2
+      min_size       = 3
       max_size       = 3
-      desired_size   = 2
+      desired_size   = 3
       labels         = { role = "security" }
     }
   }
