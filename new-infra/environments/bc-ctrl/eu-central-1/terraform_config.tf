512c1859b14827cb5ee32a3d40a54ee602f2ad7d
Afonso Costa <afonso3marques@gmail.com>
2026-04-16 23:20:48 +0200

    feat(ctrl): provision self-signed ACM cert for wazuh.bc-ctrl.internal dashboard ALB

--- new-infra/environments/bc-ctrl/eu-central-1/terraform_config.tf
+++ new-infra/environments/bc-ctrl/eu-central-1/terraform_config.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.32"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 
   backend "s3" {
