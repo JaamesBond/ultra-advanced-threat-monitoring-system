52ea7cc4098800f3c5425cdd1cbf755052d0a73e
Norbert Knez <knez.norbert@gmail.com>
2026-05-11 23:04:34 +0200

    creating cheap test ec2 to see what we can do soar wise with it

--- new-infra/environments/bc-ctrl/eu-central-1/lambda.tf
+++ new-infra/environments/bc-ctrl/eu-central-1/lambda.tf
@@ -60,3 +60,45 @@ resource "aws_iam_role_policy_attachment" "lambda_quarantine_ec2_attach" {
   role       = aws_iam_role.lambda_quarantine_ec2_role.name
   policy_arn = aws_iam_policy.lambda_quarantine_ec2_policy.arn
 }
+
+resource "aws_security_group" "cheap_test_ec2_sg" {
+  name        = "cheap-test-ec2-sg"
+  description = "Temporary test SG with basic random rules"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "cheap_test_ubuntu" {
+  ami                         = data.aws_ami.ubuntu-2404.id
+  instance_type               = "t3.nano"
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.cheap_test_ec2_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "cheap-test-ubuntu-soar-test"
+  }
+}
+
