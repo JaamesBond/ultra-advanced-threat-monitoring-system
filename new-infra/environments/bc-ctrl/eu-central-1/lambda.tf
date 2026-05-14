5091dfadd0b7b61c48ee42ba827bc776af2cba4c
Norbert Knez <knez.norbert@gmail.com>
2026-05-14 14:07:46 +0200

    ami data fix

--- new-infra/environments/bc-ctrl/eu-central-1/lambda.tf
+++ new-infra/environments/bc-ctrl/eu-central-1/lambda.tf
@@ -1,3 +1,17 @@
+data "aws_ami" "ubuntu-2404" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_security_group" "quarantine_ec2_sg" {
   name        = "quarantine-sg"
   description = "Isolates compromised EC2 instances by denying all inbound and outbound traffic"
